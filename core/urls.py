0977a287b5125fb42e1c346671bd72939777b629
DenerBley <deneriot@outlook.com>
2024-08-12 14:08:58 -0300

    Primeiro commit

---
+++ core/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('usuarios/', include('usuarios.urls')),
+    path('empresarios/', include('empresarios.urls')),
+    path('investidores/', include('investidores.urls')),
+
+]
